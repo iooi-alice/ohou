4e1f4cbc5bfc45a1916949f1044cb859e69b8525
iooi-alice <kimhee21c@hanmail.net>
2024-08-24 21:31:26 +0900

    chore: 각 이벤트의 주석 설명 추가

--- js/sidebar-drawer.js
+++ js/sidebar-drawer.js
@@ -1,11 +1,15 @@
-const drawerMenuButtonList = document.querySelectorAll('.drawer-menu .drawer-menu-button');
+const drawerMenuButtons = document.querySelectorAll('.drawer-menu .drawer-menu-button');
 
 function toggleDrawerMenu() {
+  // 2. 버튼의 부모인 drawer-menu 찾기
   const drawerMenu = this.parentNode;
+
+  // 3. 클래스 is-open, is-active 추가
   drawerMenu.classList.toggle('is-open');
   drawerMenu.classList.toggle('is-active');
 }
 
-drawerMenuButtonList.forEach(function (button) {
+// 1. 각각의 버튼 클릭 시
+drawerMenuButtons.forEach((button) => {
   button.addEventListener('click', toggleDrawerMenu);
 });
