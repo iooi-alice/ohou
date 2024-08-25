811a5ba41db7c54a4f4d7f960a7506b0e935351c
iooi-alice <kimhee21c@hanmail.net>
2024-08-26 03:39:18 +0900

    fix: 스크롤에 따라 tab의 active 상태가 순차적으로 변경되어 적용되는 문제 해결

--- js/product-tab.js
+++ js/product-tab.js
@@ -5,14 +5,20 @@ const TOP_HEADER_DESKTOP = 80 + 50 + 54;
 const TOP_HEADER_MOBILE = 50 + 40 + 40;
 
 let currentActiveTab = productTab.querySelector('.is-active');
+let disableUpdating = false;
 
 function toggleActiveTab() {
   const tabItem = this.parentNode;
 
   if (currentActiveTab !== tabItem) {
+    disableUpdating = true;
     tabItem.classList.add('is-active');
     currentActiveTab.classList.remove('is-active');
     currentActiveTab = tabItem;
+
+    setTimeout(() => {
+      disableUpdating = false;
+    }, 1000);
   }
 }
 
@@ -30,7 +36,7 @@ productTabButtonList.forEach((button) => {
   button.addEventListener('click', scrollToTabPanel);
 });
 
-// section의 위치 : window.scrollY + element.getBoundingClientRect().top
+// Panel 위치 : window.scrollY + element.getBoundingClientRect().top
 const productTabPanelIdList = [
   'product-spec',
   'product-review',
@@ -46,16 +52,19 @@ const productTabPanelList = productTabPanelIdList.map((panelId) => {
 
 const productTabPanelPositionMap = {};
 
-// tabPanel의 위치를 찾는 함수
+// tabPanel의 위치를 감지하는 함수
 function detectTabPanelPosition() {
   productTabPanelList.forEach((panel) => {
     const id = panel.getAttribute('id');
     const position = window.scrollY + panel.getBoundingClientRect().top;
     productTabPanelPositionMap[id] = position;
   });
+
+  updateActiveTabOnScroll();
 }
 
 function updateActiveTabOnScroll() {
+  if (disableUpdating) return;
   const scrolledAmount = window.scrollY + (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP + 80 : TOP_HEADER_MOBILE + 8);
 
   let newActiveTab;
@@ -72,17 +81,25 @@ function updateActiveTabOnScroll() {
     newActiveTab = productTabButtonList[0];
   }
 
+  const bodyHeight = document.body.offsetHeight + (window.innerWidth < 1200 ? 56 : 0);
+
+  if (window.scrollY + window.innerHeight === bodyHeight) {
+    newActiveTab = productTabButtonList[4];
+  }
+
   if (newActiveTab) {
     newActiveTab = newActiveTab.parentNode;
 
     if (newActiveTab !== currentActiveTab) {
       newActiveTab.classList.add('is-active');
-      currentActiveTab.classList.remove('is-active');
+      if (currentActiveTab) {
+        currentActiveTab.classList.remove('is-active');
+      }
       currentActiveTab = newActiveTab;
     }
   }
 }
 
 window.addEventListener('load', detectTabPanelPosition);
-window.addEventListener('resize', detectTabPanelPosition);
-window.addEventListener('scroll', updateActiveTabOnScroll);
+window.addEventListener('resize', _.throttle(detectTabPanelPosition, 1000));
+window.addEventListener('scroll', _.throttle(updateActiveTabOnScroll, 300));
